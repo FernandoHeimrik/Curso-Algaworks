71101e2e7c206040eeaa2c229d40a4c585b8de3a
Fernando Henrique <fernando.hnrq@gmail.com>
2019-07-21 00:17:54 -0300

    Post Categoria e BuscaPorId

--- algamoney-api/src/main/java/com/fhzalves/algamoneyapi/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/resource/CategoriaResource.java
@@ -1,11 +1,19 @@
 package com.fhzalves.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fhzalves.algamoneyapi.model.Categoria;
 import com.fhzalves.algamoneyapi.repository.CategoriaRepository;
@@ -14,11 +22,30 @@ import com.fhzalves.algamoneyapi.repository.CategoriaRepository;
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
-	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
+	@Autowired
+	public CategoriaResource(CategoriaRepository categoriaRepository) {
+		this.categoriaRepository = categoriaRepository;
+	}
+
 	@GetMapping
-	public List<Categoria> listar(){
+	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
+	}
+
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria /*,HttpServletResponse response*/) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categoriaSalva.getId()).toUri();
+		//response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{id}")
+	public Categoria buscarPorId(@PathVariable Long id) {
+		return categoriaRepository.findById(id).orElse(null);
 	}
 }
