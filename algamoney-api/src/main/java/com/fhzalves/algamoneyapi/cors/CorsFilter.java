69db41709ded6fb74c0f6a221aad14c89b2f9ccd
Fernando Alves <fernando.hnrq@gmail.com>
2019-07-31 20:58:49 -0300

    Criando filtro para CORS

---
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/cors/CorsFilter.java
@@ -0,0 +1,46 @@
+package com.fhzalves.algamoneyapi.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+	private String originPermitido = "http://localhost:8000";// TODO: Configurar para diferentes ambientes
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+
+		response.setHeader("Access-Control-Allow-Origin", originPermitido);
+		response.setHeader("Access-Control-Allow-Credentials", "true");// Para enviar o Cookie
+
+		if ("OPTIONS".equals(request.getMethod()) && originPermitido.equals(request.getHeader("Origin"))) {
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			response.setHeader("Access-Control-Max-Age", "3600");// 1h
+
+			response.setStatus(HttpServletResponse.SC_OK);
+
+		} else {
+			chain.doFilter(request, response);
+		}
+
+	}
+
+}
