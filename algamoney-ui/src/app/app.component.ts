3a7b25724a557497496b5dbf924ab00a09b56fa8
Fernando Alves <fernando.hnrq@gmail.com>
2019-09-30 00:12:49 -0300

    Adicionando uma tabela de dados

--- algamoney-ui/src/app/app.component.ts
+++ algamoney-ui/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'algamoney-ui';
+  lancamentos = [
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/09/2019',
+      dataPagamento: null, valor: 4.55, pessoa:'Padaria do José'},
+    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/09/2019',
+      dataPagamento: '09/06/2019', valor: 80000, pessoa: 'Atacado Brasil'},
+    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '20/09/2019',
+      dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda'},
+    { tipo: 'DESPESA', descricao: 'Mensalidade da escola', dataVencimento: '10/09/2019',
+      dataPagamento: '09/06/2019', valor: 800, pessoa: 'Escola'}
+  ]
 }
