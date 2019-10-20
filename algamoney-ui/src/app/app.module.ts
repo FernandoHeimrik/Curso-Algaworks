eae773b2f4e2909d31527f97d5d3093f73f8d23e
Fernando Alves <fernando.hnrq@gmail.com>
2019-10-20 00:55:06 -0300

    Validando controles de formulário com PrimeNG

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
@@ -32,6 +33,7 @@ import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.co
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     InputTextModule,
     ButtonModule,
     TableModule,
