ad16bc7e6b34370e378bb3608c137d2817420afa
Fernando Henrique <fernando.hnrq@gmail.com>
2019-11-10 18:53:21 -0300

    Tratando acessos de usuários deslogados na AuthGuard

--- algamoney-ui/src/app/seguranca/auth.guard.ts
+++ algamoney-ui/src/app/seguranca/auth.guard.ts
@@ -11,10 +11,21 @@ export class AuthGuard implements CanActivate {
   constructor(
     private auth: AuthService,
     private router: Router
-  ){}
+  ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    if (route.data.roles && !this.auth.temQualquerPermissao(route.data.roles)) {
+
+    if (this.auth.isAccessTokenInvalido()) {
+      console.log('Navegação com access token inválido. Obtendo novo token...');
+      return this.auth.obterNovoAccessToken()
+        .then(() => {
+          if (this.auth.isAccessTokenInvalido()) {
+            this.router.navigate(['/login']);
+            return false;
+          }
+          return true;
+        });
+    } else if (route.data.roles && !this.auth.temQualquerPermissao(route.data.roles)) {
       this.router.navigate(['/nao-autorizado']);
       return false;
     }
