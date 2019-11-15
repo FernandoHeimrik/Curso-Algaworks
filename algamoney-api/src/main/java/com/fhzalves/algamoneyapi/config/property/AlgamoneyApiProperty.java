b84239da5a31cc01fb70beb138c0bd8166b85c6f
Fernando Alves <fernando.hnrq@gmail.com>
2019-11-15 17:36:38 -0300

    Finalizacao para Deploy

--- algamoney-api/src/main/java/com/fhzalves/algamoneyapi/config/property/AlgamoneyApiProperty.java
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/config/property/AlgamoneyApiProperty.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 
-	private String originPermitida = "http://localhost:4200";
+	private String originPermitida = "https://curso-algamoney-ui.herokuapp.com";
 
 	private final Seguranca seguranca = new Seguranca();
 
