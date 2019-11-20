edb6d205aa763017a1ebdc4bcb9decbb41560dc8
Fernando Alves <fernando.hnrq@gmail.com>
2019-11-20 00:47:33 -0300

    Retornando os dados estatísticos de lançamento por categoria

--- algamoney-api/src/main/java/com/fhzalves/algamoneyapi/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/resource/LancamentoResource.java
@@ -1,5 +1,6 @@
 package com.fhzalves.algamoneyapi.resource;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fhzalves.algamoneyapi.dto.LancamentoEstatisticaCategoria;
 import com.fhzalves.algamoneyapi.event.RecursoCriadoEvent;
 import com.fhzalves.algamoneyapi.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.fhzalves.algamoneyapi.model.Lancamento;
@@ -50,6 +52,12 @@ public class LancamentoResource {
 
 	@Autowired
 	private MessageSource messageSource;
+	
+	@GetMapping("/estatisticas/por-categoria")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+	public List<LancamentoEstatisticaCategoria> porCategoria(){
+		return this.lancamentoRepository.porCategoria(LocalDate.now());
+	}
 
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
--- algamoney-api/src/main/resources/db/migration/V03__criar_e_registrar_lancamentos.sql
+++ algamoney-api/src/main/resources/db/migration/V03__criar_e_registrar_lancamentos.sql
@@ -12,18 +12,18 @@ CREATE TABLE lancamento (
 	FOREIGN KEY (id_pessoa) REFERENCES pessoa(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Salário mensal', '2019-06-10', null, 6500.00, 'Distribuição de lucros', 'RECEITA', 1, 1);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Bahamas', '2019-02-10', '2019-02-10', 100.32, null, 'DESPESA', 2, 2);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Top Club', '2019-06-10', null, 120, null, 'RECEITA', 3, 3);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('CEMIG', '2019-02-10', '2019-02-10', 110.44, 'Geração', 'RECEITA', 3, 4);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('DMAE', '2019-06-10', null, 200.30, null, 'DESPESA', 3, 5);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Extra', '2019-03-10', '2019-03-10', 1010.32, null, 'RECEITA', 4, 6);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Bahamas', '2019-06-10', null, 500, null, 'RECEITA', 1, 7);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Top Club', '2019-03-10', '2019-03-10', 400.32, null, 'DESPESA', 4, 8);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Despachante', '2019-06-10', null, 123.64, 'Multas', 'DESPESA', 3, 9);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Pneus', '2019-04-10', '2019-04-10', 665.33, null, 'RECEITA', 5, 10);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Café', '2019-06-10', null, 8.32, null, 'DESPESA', 1, 5);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Eletrônicos', '2019-04-10', '2019-04-10', 2100.32, null, 'DESPESA', 5, 4);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Instrumentos', '2019-06-10', null, 1040.32, null, 'DESPESA', 4, 3);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Café', '2019-04-10', '2019-04-10', 4.32, null, 'DESPESA', 4, 2);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Lanche', '2019-06-10', null, 10.20, null, 'DESPESA', 4, 1);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Salário mensal', '2019-11-10', null, 6500.00, 'Distribuição de lucros', 'RECEITA', 1, 1);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Bahamas', '2019-11-10', '2019-11-10', 100.32, null, 'DESPESA', 2, 2);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Top Club', '2019-11-10', null, 120, null, 'RECEITA', 3, 3);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('CEMIG', '2019-11-10', '2019-11-10', 110.44, 'Geração', 'RECEITA', 3, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('DMAE', '2019-11-10', null, 200.30, null, 'DESPESA', 3, 5);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Extra', '2019-11-10', '2019-11-10', 1010.32, null, 'RECEITA', 4, 6);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Bahamas', '2019-11-10', null, 500, null, 'RECEITA', 1, 7);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Top Club', '2019-11-10', '2019-11-10', 400.32, null, 'DESPESA', 4, 8);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Despachante', '2019-11-10', null, 123.64, 'Multas', 'DESPESA', 3, 9);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Pneus', '2019-11-10', '2019-11-10', 665.33, null, 'RECEITA', 5, 10);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Café', '2019-11-10', null, 8.32, null, 'DESPESA', 1, 5);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Eletrônicos', '2019-11-10', '2019-11-10', 2100.32, null, 'DESPESA', 5, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Instrumentos', '2019-11-10', null, 1110.32, null, 'DESPESA', 4, 3);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Café', '2019-11-10', '2019-11-10', 4.32, null, 'DESPESA', 4, 2);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, id_categoria, id_pessoa) values ('Lanche', '2019-11-10', null, 10.20, null, 'DESPESA', 4, 1);
