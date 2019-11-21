7db3e44ce8a5f2cab16b528d06cdabb9f6c1b8d1
Fernando Alves <fernando.hnrq@gmail.com>
2019-11-20 23:17:37 -0300

    Criando o DTO do relatório

---
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/dto/LancamentoEstatisticaPessoa.java
@@ -0,0 +1,46 @@
+package com.fhzalves.algamoneyapi.dto;
+
+import java.math.BigDecimal;
+
+import com.fhzalves.algamoneyapi.model.Pessoa;
+import com.fhzalves.algamoneyapi.model.enuns.TipoLancamento;
+
+public class LancamentoEstatisticaPessoa {
+
+	private TipoLancamento tipo;
+
+	private Pessoa pessoa;
+
+	private BigDecimal total;
+
+	public LancamentoEstatisticaPessoa(TipoLancamento tipo, Pessoa pessoa, BigDecimal total) {
+		this.tipo = tipo;
+		this.pessoa = pessoa;
+		this.total = total;
+	}
+
+	public TipoLancamento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoLancamento tipo) {
+		this.tipo = tipo;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+}
--- algamoney-api/src/main/java/com/fhzalves/algamoneyapi/model/enuns/TipoLancamento.java
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/model/enuns/TipoLancamento.java
@@ -1,6 +1,15 @@
 package com.fhzalves.algamoneyapi.model.enuns;
 
 public enum TipoLancamento {
-	RECEITA,
-	DESPESA
+	RECEITA("Receita"), DESPESA("Despesa");
+
+	private final String descricao;
+
+	TipoLancamento(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 }
