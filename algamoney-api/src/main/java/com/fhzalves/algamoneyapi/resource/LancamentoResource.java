ef5d89b99e9452bc073d70c31b391dd2d939ef69
Fernando Alves <fernando.hnrq@gmail.com>
2019-11-28 23:28:23 -0300

    Upload de arquivos para API

--- algamoney-api/src/main/java/com/fhzalves/algamoneyapi/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/resource/LancamentoResource.java
@@ -1,5 +1,8 @@
 package com.fhzalves.algamoneyapi.resource;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -30,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.fhzalves.algamoneyapi.dto.LancamentoEstatisticaCategoria;
 import com.fhzalves.algamoneyapi.dto.LancamentoEstatisticaDia;
@@ -57,6 +61,17 @@ public class LancamentoResource {
 
 	@Autowired
 	private MessageSource messageSource;
+	
+	
+	@PostMapping("/anexo")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
+	public String uploadAnexo(@RequestParam MultipartFile anexo) throws IOException {
+		OutputStream out = new FileOutputStream(
+				"C:/Users/Fernando/Pictures/anexo--" + anexo.getOriginalFilename());
+		out.write(anexo.getBytes());
+		out.close();
+		return "ok";
+	}
 	
 	@GetMapping("/relatorios/por-pessoa")
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
