541de2cb0ed3317d568c577d1087e43c432c263a
Fernando Alves <fernando.hnrq@gmail.com>
2019-11-28 23:06:16 -0300

    Usando a propriedade orphanRemoval

--- algamoney-api/src/main/java/com/fhzalves/algamoneyapi/service/PessoaService.java
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/service/PessoaService.java
@@ -21,8 +21,12 @@ public class PessoaService {
 	
 	public Pessoa atualizar(Long id, Pessoa pessoa) {
 		Pessoa pessoaSalva = buscarPessoaPorId(id);
-		pessoa.getContatos().forEach(c -> c.setPessoa(pessoa));
-		BeanUtils.copyProperties(pessoa, pessoaSalva, "id");
+		
+		pessoaSalva.getContatos().clear();
+		pessoaSalva.getContatos().addAll(pessoa.getContatos());
+		pessoaSalva.getContatos().forEach(c -> c.setPessoa(pessoaSalva));
+		
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "id", "contatos");
 		return pessoaRepository.save(pessoaSalva);
 	}
 
