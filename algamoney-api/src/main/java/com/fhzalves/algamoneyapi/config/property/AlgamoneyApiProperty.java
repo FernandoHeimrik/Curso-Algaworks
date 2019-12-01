14eb8101273440cc061ee919ac6b5eece8e3ddb5
Fernando Henrique <fernando.hnrq@gmail.com>
2019-12-01 16:41:24 -0300

    Configurando o serviço S3

--- algamoney-api/src/main/java/com/fhzalves/algamoneyapi/config/property/AlgamoneyApiProperty.java
+++ algamoney-api/src/main/java/com/fhzalves/algamoneyapi/config/property/AlgamoneyApiProperty.java
@@ -10,6 +10,12 @@ public class AlgamoneyApiProperty {
 	private final Seguranca seguranca = new Seguranca();
 
 	private final Mail mail = new Mail();
+	
+	private final S3 s3 = new S3();
+	
+	public S3 getS3() {
+		return s3;
+	}
 
 	public Mail getMail() {
 		return mail;
@@ -25,6 +31,29 @@ public class AlgamoneyApiProperty {
 
 	public void setOriginPermitida(String originPermitida) {
 		this.originPermitida = originPermitida;
+	}
+
+	public static class S3 {
+
+		private String accessKeyId;
+
+		private String secretAccessKey;
+
+		public String getAccessKeyId() {
+			return accessKeyId;
+		}
+
+		public void setAccessKeyId(String accessKeyId) {
+			this.accessKeyId = accessKeyId;
+		}
+
+		public String getSecretAccessKey() {
+			return secretAccessKey;
+		}
+
+		public void setSecretAccessKey(String secretAccessKey) {
+			this.secretAccessKey = secretAccessKey;
+		}
 	}
 
 	public static class Seguranca {
