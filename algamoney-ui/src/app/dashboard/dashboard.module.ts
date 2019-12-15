2d5685dd3d46b5c403f69496423b60cd334a9149
Fernando Henrique <fernando.hnrq@gmail.com>
2019-12-14 21:41:57 -0300

    Plotando gráficos com dados estáticos

--- algamoney-ui/src/app/dashboard/dashboard.module.ts
+++ algamoney-ui/src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {PanelModule} from 'primeng/panel';
+import {ChartModule} from 'primeng/chart';
+
 import { SharedModule } from './../shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -10,6 +13,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
+    PanelModule,
+    ChartModule,
+
     SharedModule,
     DashboardRoutingModule
   ]
