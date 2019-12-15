6502e5796489495f84e65f8756c8af7c5d59b1a7
Fernando Henrique <fernando.hnrq@gmail.com>
2019-12-15 16:07:42 -0300

    Formatando labels no Chart.JS

--- algamoney-ui/src/app/dashboard/dashboard/dashboard.component.ts
+++ algamoney-ui/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 
 import { DashboardService } from './../dashboard.service';
 
@@ -12,8 +13,23 @@ export class DashboardComponent implements OnInit {
   pieChartData: any;
   lineChartData: any;
 
+  options = {
+    tooltips: {
+      callbacks: {
+        label: (tooltipItem, data) => {
+          const dataset = data.datasets[tooltipItem.datasetIndex];
+          const valor = dataset.data[tooltipItem.index];
+          const label = dataset.label ? (dataset.label + ': ') : '';
+
+          return label + this.decimalPipe.transform(valor, '1.2-2');
+        }
+      }
+    }
+  };
+
   constructor(
-    private dashboardService: DashboardService
+    private dashboardService: DashboardService,
+    private decimalPipe: DecimalPipe
   ) { }
 
   ngOnInit() {
