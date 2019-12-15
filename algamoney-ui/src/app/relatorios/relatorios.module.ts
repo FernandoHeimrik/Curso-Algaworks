b5820fc9d0a361d1bb31e0d3b6fbfb09f4225277
Fernando Henrique <fernando.hnrq@gmail.com>
2019-12-15 16:36:33 -0300

    Configurando formulário do relatório

--- algamoney-ui/src/app/relatorios/relatorios.module.ts
+++ algamoney-ui/src/app/relatorios/relatorios.module.ts
@@ -1,5 +1,8 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { CalendarModule } from 'primeng/calendar';
 
 import { SharedModule } from './../shared/shared.module';
 import { RelatoriosRoutingModule } from './relatorios-routing.module';
@@ -10,7 +13,10 @@ import { RelatorioLancamentosComponent } from './relatorio-lancamentos/relatorio
   declarations: [RelatorioLancamentosComponent],
   imports: [
     CommonModule,
+    FormsModule,
 
+    CalendarModule,
+    
     SharedModule,
     RelatoriosRoutingModule
   ]
