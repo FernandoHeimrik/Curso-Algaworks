eaa836f43239ee903912c803f2160c2e33746612
Fernando Henrique <fernando.hnrq@gmail.com>
2019-12-17 22:29:49 -0300

    Editando contato

--- algamoney-ui/src/app/pessoas/pessoas-cadastro/pessoas-cadastro.component.ts
+++ algamoney-ui/src/app/pessoas/pessoas-cadastro/pessoas-cadastro.component.ts
@@ -19,6 +19,7 @@ export class PessoasCadastroComponent implements OnInit {
   pessoa = new Pessoa();
   exibindoFormularioContato = false;
   contato: Contato;
+  contatoIndex: number;
 
   constructor(
     private pessoaService: PessoaService,
@@ -43,10 +44,17 @@ export class PessoasCadastroComponent implements OnInit {
   prepararNovoContato() {
     this.exibindoFormularioContato = true;
     this.contato = new Contato();
+    this.contatoIndex = this.pessoa.contatos.length;
+  }
+
+  prepararEdicaoContato(contato: Contato, index: number) {
+    this.contato = this.clonarContato(contato);
+    this.exibindoFormularioContato = true;
+    this.contatoIndex = index;
   }
 
   confirmarContato(frm: FormControl) {
-    this.pessoa.contatos.push(this.clonarContato(this.contato));
+    this.pessoa.contatos[this.contatoIndex] = this.clonarContato(this.contato);
 
     this.exibindoFormularioContato = false;
 
